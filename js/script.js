afdec3f5d6d19a8cbb9649beb7a9e78c4a25812e
Pragyakar Joshi <pragyakar75@gmail.com>
2018-06-24 23:21:45 +0545

    API added

---
+++ js/script.js
@@ -0,0 +1,32 @@
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(function(position){
+        loadWeather(position.coords.latitude + ',' + position.coords.longitude);
+    });
+} else {
+    loadWeather('Kathmandu, NP', '');
+}
+
+$(document).ready(function(){
+    setInterval(loadWeather, 10000);
+})
+
+function loadWeather(location, woeid) {
+    $.simpleWeather({
+        location: location,
+        woeid: woeid,
+        unit: 'c',
+        success: function(weather){
+            city = weather.city;
+            country = weather.country;
+            celciusTemp = weather.temp+'&deg;C';
+            wcode = '<img src="../icons/'+weather.code+'.svg" class="icon">';  
+            wind = weather.wind.speed + ' ' + weather.units.speed;
+            humidity = weather.humidity+ '%';
+
+            $('.climate-icon').html(wcode);
+            $('.temp-cel').html(celciusTemp);
+            $('.location').html(city+', '+country);
+
+        }
+    })
+}
